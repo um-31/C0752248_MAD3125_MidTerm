6fc6e3c474f2267fd0011678fd6f51530cbdf16c
um-31 <udaymahajan97@gmail.com>
2019-08-08 18:40:27 -0400

    [Udhay] Login Screen Dsign Created.

---
+++ app/src/main/java/com/example/c0752248_mad3125_midterm/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.c0752248_mad3125_midterm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private Button btnLogin;
+    private EditText edtUserName;
+    private EditText edtPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
