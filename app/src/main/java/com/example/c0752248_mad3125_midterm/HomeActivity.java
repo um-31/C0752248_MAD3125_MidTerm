0073c7a161312188b4c8ad2622bb1687421ee99a
um-31 <udaymahajan97@gmail.com>
2019-08-08 21:23:02 -0400

    [Udhay] Login screen Fully Working.

---
+++ app/src/main/java/com/example/c0752248_mad3125_midterm/HomeActivity.java
@@ -0,0 +1,14 @@
+package com.example.c0752248_mad3125_midterm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
--- app/src/main/java/com/example/c0752248_mad3125_midterm/MainActivity.java
+++ app/src/main/java/com/example/c0752248_mad3125_midterm/MainActivity.java
@@ -5,9 +5,11 @@ import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -22,10 +24,15 @@ public class MainActivity extends AppCompatActivity {
     private Button btnLogin;
     private EditText txtUserName;
     private EditText txtPassword;
+    int temp = 0;
     String userName;
     String password;
-    Login detail1 = new Login("admin1@gmail.com","1234");
+    String storeUsername;
+    String storePassword;
+    Login detail1 = new Login("admin1","1234");
     Login detail2 = new Login("admin2@gmail.com","1234");
+    Login detail3 = new Login("admin3@gmail.com","1234");
+    Login[] login = {detail1,detail2,detail3};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +45,24 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 userName = txtUserName.getText().toString();
                 password = txtPassword.getText().toString();
-                login(userName,password);
+                if(userName.isEmpty() || userName.trim().length() == 0)
+                {
+                    txtUserName.setError("Enter Email ID");
+                    temp++;
+                }else {
+                    temp = 0;
+                }
+                if(password.isEmpty() || password.trim().length() == 0)
+                {
+                    txtPassword.setError("Enter Password");
+                    temp++;
+                }
+                else{
+                    temp = 0;
+                }
+                if(temp == 0) {
+                    login(userName,password);
+                }
             }
         });
     }
@@ -48,6 +72,22 @@ public class MainActivity extends AppCompatActivity {
         txtPassword = (EditText) findViewById(R.id.txtPassword);
     }
     public void login(String userName,String password) {
+        temp = 0;
 
+        for(int i = 0;i<login.length;i++) {
+            storeUsername = login[i].userName;
+            storePassword = login[i].password;;
+
+            if (userName.equals(storeUsername) && password.equals(storePassword)) {
+                Intent intent = new Intent(MainActivity.this,HomeActivity.class);
+                startActivity(intent);
+            }else {
+                temp++;
+            }
+
+        }
+        if (temp != 0) {
+            Toast.makeText(this, "Wrong UserID or Password", Toast.LENGTH_SHORT).show();
+        }
     }
 }
